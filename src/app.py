13707b94bcfa370a94a1d5f38f10c3f612f2c40b
Martín Suárez <ms.prgramador@gmail.com>
2021-10-19 00:28:59 +0000

    doing exercise 7.1

--- src/app.py
+++ src/app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, json
 app = Flask(__name__)
+
+
+todos = [
+    {"label" : "My first task", "done":False},
+    {"label" : "My first task2", "done":False}
+]
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(request_body)
+    todos_text = jsonify(todos)
+    print("Incoming request with the following body", request_body)
+    return todos_text
 
 
 if __name__ == '__main__':
